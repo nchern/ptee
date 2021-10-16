c2ce8b80f5e94b0d95e677c2099cedb910898948
nchern <nchern@inbox.ru>
2021-10-16 22:25:43 +0200

    add an ability to redirect stdout of a target program to a file

--- main.go
+++ main.go
@@ -8,12 +8,15 @@ import (
 	"os/exec"
 )
 
+var outputFile = flag.String("o", "", "filename to redirect output of a given command to")
+
 func init() {
 	flag.Usage = func() {
 		fmt.Println("Starts a process(the 1st argument specifies the command to run),")
 		fmt.Println("connects stdin to this process' stdin and mirrors it to stdout as well.")
 		fmt.Println()
 		fmt.Printf("Usage: %s [command] [args...]\n", os.Args[0])
+		flag.PrintDefaults()
 	}
 	flag.Parse()
 	if len(os.Args) < 2 {
@@ -22,13 +25,25 @@ func init() {
 	}
 }
 
-func main() {
+func run() error {
 	in := io.TeeReader(os.Stdin, os.Stdout)
-	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd := exec.Command(flag.Args()[0], flag.Args()[1:]...)
 	cmd.Stdin = in
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
+	if *outputFile != "" {
+		f, err := os.Create(*outputFile)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		cmd.Stdout = f
+	}
+	return cmd.Run()
+}
+
+func main() {
+	if err := run(); err != nil {
 		if exitError, ok := err.(*exec.ExitError); ok {
 			os.Exit(exitError.ExitCode())
 		}
